a07aa9cbe6081261d80adef6119e836a43ea8988
Yixuan Lin <yixuan.lin1993@outlook.com>
2023-07-24 19:19:43 +0800

    add meta data

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log('Loading function');
+// ==UserScript==
+// @name         Hello Tempermonkey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new script
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Hello, World!');
+})();
